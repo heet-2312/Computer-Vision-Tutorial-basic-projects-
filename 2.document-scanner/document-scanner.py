dc56cccfd55a90101be986d89e38655970779772
heet-2312 <51704238+heet-2312@users.noreply.github.com>
2020-05-13 01:56:33 +0530

    Add files via upload

---
+++ 2.document-scanner/document-scanner.py
@@ -0,0 +1,62 @@
+# import the necessary packages
+from pyimagesearch.transform import four_point_transform
+from skimage.filters import threshold_local
+import numpy as np
+import argparse
+import cv2
+import imutils
+
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True,
+	help = "Path to the image to be scanned")
+args = vars(ap.parse_args())
+
+# load the image and compute the ratio of the old height
+# to the new height, clone it, and resize it to speed up the image processing
+#we maintain the copy so that scanning is performed on the original image
+image = cv2.imread(args["image"])
+ratio = image.shape[0] / 500.0
+orig = image.copy()
+image = imutils.resize(image, height = 500)
+# convert the image to grayscale, blur it, and find edges
+# in the image
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#perform gaussian blurring to remove high frquency noise
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+#canny edge detection
+edged = cv2.Canny(gray, 75, 200)
+
+#we perform the below operations by assuming that the piece of paper on analysis is done has four edges
+# find the contours in the edged image, keeping only the
+# largest ones, and initialize the screen contour
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+#sort the counters by area and keep only the largest ones.This allows us to only examine the largest of the contours,
+# discarding the rest.
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+# loop over the contours
+for c in cnts:
+	# approximate the contour
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+	# if our approximated contour has four points, then we
+	# can assume that we have found our screen
+	if len(approx) == 4:
+		screenCnt = approx
+		break
+
+#the first is our original image we loaded off disk
+# and the second argument is the contour representing the document, multiplied by the resized ratio
+warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+# convert the warped image to grayscale, then threshold it
+# to give it that 'black and white' paper effect
+warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+T = threshold_local(warped, 11, offset = 10, method = "gaussian")
+warped = (warped > T).astype("uint8") * 255
+# show the original and scanned images
+cv2.imshow("Original", imutils.resize(orig, height = 650))
+cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+cv2.waitKey(0)
